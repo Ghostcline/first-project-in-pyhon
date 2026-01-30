85e4018531dfef9d171681f607332fbae74c1773
abdualrhman-ai <dhwwwmalmlk@gmail.com>
2026-01-30 22:53:45 +0800

    Add frist simple project in python

---
+++ first project.py
@@ -0,0 +1,28 @@
+import random
+
+print("Welcome to number guessing game.\n")
+right_number = random.randint(1, 21)
+
+print("I've picked a number for you to guess.")
+print("The number is between 1 and 20.\n")
+
+guessed_number = int(input("Guess a number: "))
+
+while guessed_number != right_number:
+
+    if guessed_number < 1 or guessed_number > 21:
+        print("Invalid guess. Please enter a number between 1 and 20.")
+
+
+    elif guessed_number > right_number:
+        print("\nYour guess is not correct.")
+        print("Give it another shot.")
+        print("Choose a lower number.\n")
+    else:
+        print("\nYour guess is not correct.")
+        print("Give it another shot.")
+        print("Choose a higher number.\n")
+
+    guessed_number = int(input("Enter guess number again: "))
+print("\nCorrect guess.")
+print("You won. Thank you for playing.")
